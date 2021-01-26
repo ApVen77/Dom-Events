8842d7c339a3408ef6cf7d9749f2d6fd25eb64e3
April Long <april11dance@gmail.com>
2021-01-25 22:25:37 -0700

    all the changes

---
+++ mouseEvents/app.js
@@ -0,0 +1,17 @@
+// Use an addEventListener
+//  to attach a "mouseover" and "mouseout" event to a h3 element.
+
+
+
+//Part II
+// Use an addEventListener
+//  to attach a "mouseover" and "mouseout" element that asks 
+// and answers a trivia question
+function mOver(obj) {
+    obj.innerHTML = "Albany"
+}
+
+function mOut(obj) {
+    obj.innerHTML = "What is the capital of NY?"
+}
+
---
+++ userInterface/app.js
@@ -0,0 +1,7 @@
+// Using onload on an <img> element.
+//  Alert "Must be 18+" immediately after an image has been 
+//  loaded:
+
+function loadImage() {
+    alert("how old are you?");
+  }
